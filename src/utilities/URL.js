ac724437dddf3179633a0fb2dc1756c47da9f41d
Abdullah Akram <108747089+aharoo786@users.noreply.github.com>
2026-08-06 12:33:19 +0500

    chore: point admin API to stagelaundry on dev

--- src/utilities/URL.js
+++ src/utilities/URL.js
@@ -1,8 +1,9 @@
-// export const BASE_URL = " http://localhost:8083/";
-export const BASE_URL = import.meta.env.LAUNDRY_API_BASE_URL || "https://prodlaundry.sigisolutions.net/";
+// export const BASE_URL = "http://localhost:8083/";
+// export const BASE_URL = "https://prodlaundry.sigisolutions.net/";
+export const BASE_URL =
+  import.meta.env.LAUNDRY_API_BASE_URL ||
+  "https://stagelaundry.sigisolutions.net/";
 
-// export const BASE_URL = "https://stagelaundry.sigisolutions.net"
-export const googleApiKey = import.meta.env.LAUNDRY_GOOGLE_MAPS_KEY || "AIzaSyADTqd6DhbPp9HHY93FzP4ySblD4fx-bBE";
-
-
-
+export const googleApiKey =
+  import.meta.env.LAUNDRY_GOOGLE_MAPS_KEY ||
+  "AIzaSyADTqd6DhbPp9HHY93FzP4ySblD4fx-bBE";
